c3d309b5d498ea3f3bf709cca2b60cb982ec659f
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-07-25 20:02:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	if !(self.end_with?("."))
+  		false
+  	else 
+  		true
+  	end
+  	
   end
 
   def question?
-
+  	if !(self.end_with?("?"))
+  		false
+  	else 
+  		true
+  	end
+  
   end
 
   def exclamation?
-
+  	if !(self.end_with?("!"))
+  		false
+  	else 
+  		true
+  	end
+  	
   end
 
   def count_sentences
-
+  	new_str = self.gsub("!", ".").gsub("?",".").gsub("!!",".").split(".")
+    count =  0
+    new_str.each do |sentence|
+      if sentence =~ /[A-Za-z]/
+        count += 1
+      end 
+    end
+      count 
   end
 end
